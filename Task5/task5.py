074877315e8f57abf015bb488e4b4b4d80f580c8
mohammedYasser11 <2100921@eng.asu.edu.eg>
2024-03-03 00:55:22 +0200

    remove unused functions

--- Task5/task5.py
+++ Task5/task5.py
@@ -1,25 +1,3 @@
-def greedy_minimum_moves(n):
-    if n % 2 != 0:
-        return "No solution2"
-    
-    pairs = n // 2
-    total_moves = 0
-    remaining_moves = pairs
-
-    for i in range(1, n+1):
-        if remaining_moves <= 0:
-            break
-        
-        if remaining_moves < i:
-            moves_this_step = remaining_moves
-        else:
-            moves_this_step = i
-        
-        total_moves += moves_this_step
-        remaining_moves -= moves_this_step
-
-    return total_moves
-
 def greedy_pair_coins(lstOfN):
     for n in lstOfN:
         lst = [1] *n
