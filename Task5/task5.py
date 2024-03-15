f884a87b4d4b1e8beaef266ea1131aba9f276e66
Saraashour74 <21p0337@eng.asu.edu.eg>
2024-03-15 23:26:33 +0200

    modified

--- Task5/task5.py
+++ Task5/task5.py
@@ -8,26 +8,26 @@ def greedy_pair_coin(numCoins):
         for i in range(1,n//4,1):         #rightside
             #1,2,.. n/4-1
                 paired = False
-                index_of_current_coin=numCoins-1    #to be paired
+                index_coin_we_are_jumping_to=numCoins-1    #to be paired
                 while(not paired):
-                    while(rowOfCoins[index_of_current_coin]!=1):
-                        index_of_current_coin-=1
+                    while(rowOfCoins[index_coin_we_are_jumping_to]!=1):
+                        index_coin_we_are_jumping_to-=1
                     #now I have single coin
                     #let's see if we can pair it
                     numberCoinsToBeJumped=i
-                    index_coin_we_are_jumping_to=index_of_current_coin-1
-                    while(numberCoinsToBeJumped>0 and  index_coin_we_are_jumping_to>=0):
+                    index_of_current_coin=index_coin_we_are_jumping_to-1
+                    while(numberCoinsToBeJumped>0 and index_of_current_coin>=0):
                         # while I have to jump more coins ==> I deduct coins and jump them 
-                        numberCoinsToBeJumped-=rowOfCoins[ index_coin_we_are_jumping_to]
-                        index_coin_we_are_jumping_to-=1
+                        numberCoinsToBeJumped-=rowOfCoins[index_of_current_coin]
+                        index_of_current_coin-=1
                     if(numberCoinsToBeJumped==0):
-                        while(rowOfCoins[ index_coin_we_are_jumping_to]<1):
-                            index_coin_we_are_jumping_to-=1
+                        while(rowOfCoins[index_of_current_coin]<1):
+                            index_of_current_coin-=1
                         if(rowOfCoins[index_of_current_coin]==1):
                             #we found exactly a coin to jump to
                             paired=True
-                            rowOfCoins[index_of_current_coin]=2
-                            rowOfCoins[ index_coin_we_are_jumping_to]=0
+                            rowOfCoins[index_coin_we_are_jumping_to]=2
+                            rowOfCoins[index_of_current_coin]=0
                 print(rowOfCoins)       
         for i in range(n//4,n//2+1,1):          #leftside
                 paired = False
