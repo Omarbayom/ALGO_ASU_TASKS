2c0e6916e18d9c3c53f7f468b7818bc55b901182
Saraashour74 <125590297+Saraashour74@users.noreply.github.com>
2024-05-06 00:24:02 +0300

    update dvide and conqure

--- task4/dvideandconqure_task4,.py
+++ task4/dvideandconqure_task4,.py
@@ -1,16 +1,24 @@
-def hanoi(n, A, B, C, D):
+def _4_pegs(n, A, B, C, D):
     global total_moves
-    if n == 1:
-        print("Move disk 1 from", A, "to", B)
+    
+    if (n == 0):
+        return
+    if (n == 1):
+        print("Move disk", n, "from rod", A, "to rod", B)
         total_moves += 1
-    else:
-        k = n - 1
-        hanoi(k, A, C, D, B)
-        print("Move disk", n, "from", A, "to", B)
-        total_moves += 1
-        hanoi(k, D, B, C, A)
+        return
 
+    _4_pegs(n - 2, A, C, D, B)
+    total_moves += 3
+
+    print("Move disk", n - 1, "from rod", A, "to rod", D)
+    print("Move disk", n, "from rod", A, "to rod", B)
+    print("Move disk", n - 1, "from rod", D, "to rod", B)
+
+    _4_pegs(n - 2, C, B, A, D)
+
+n = int(input("Enter the number of disks: "))
 total_moves = 0
-n = 8
-hanoi(n, 'A', 'B', 'C', 'D')
+_4_pegs(n, 'A', 'D', 'B', 'C')
 print("Total moves:", total_moves)
+
