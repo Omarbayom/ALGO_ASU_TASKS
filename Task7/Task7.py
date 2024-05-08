646e75bf8cde5d09c9678c40226c931d35c84ea5
Omar Osama <omarosama534@gmail.com>
2024-05-08 12:46:11 +0300

    Update the solution

--- Task7/Task7.py
+++ Task7/Task7.py
@@ -1,49 +1,55 @@
 import random
 
-def randmove(target_position,n):
-    if (target_position == 1):
-        return target_position+1
-    elif (target_position == n):
-        return target_position-1
-    
-    x = random.randint(0,1)
+memo = {} 
 
-    if (x==0):
-        target_position+=1
+def randmove(target_position, n):
+    if target_position == 1:
+        return target_position + 1
+    elif target_position == n:
+        return target_position - 1
+    
+    x = random.randint(0, 1)
+
+    if x == 0:
+        target_position += 1
     else:
-        target_position-=1
+        target_position -= 1
 
     return target_position
 
-
 def simulate_game(n):
     target_position = random.randint(1, n)
-    
     shots = 0
     j = 1
-    for i in range(2, n,1):
-        print("Iteration number:", j)
-        print ("Target position at :",target_position, " Shooting position:",i,"\n")
+    hits = []  
+    
+    if target_position in memo: 
+        return memo[target_position]
+    
+    for i in range(2, n, 1):
+        hits.append((i, target_position))
         shots += 1
         if i == target_position:
-            return shots
+            memo[target_position] = (shots, hits)
+            return shots, hits
         else:
-            target_position = randmove(target_position,n)
-            j+=1
+            target_position = randmove(target_position, n)
+            j += 1
 
-    for i in range(n-1, 1, -1):
-        print("Iteration number:", j)
-        print ("Target position at :",target_position, " Shooting position: ",i,"\n")
+    for i in range(n - 1, 1, -1):
+        hits.append((i, target_position))
         shots += 1
         if i == target_position:
-            return shots
+            memo[target_position] = (shots, hits)
+            return shots, hits
         else:
-            target_position = randmove(target_position,n)
-            j+=1
+            target_position = randmove(target_position, n)
+            j += 1
 
-    return shots
+    return shots, hits
 
-
-#Test the function
+# Test the function
 n = 5
-print("Number of shots taken to hit the target for n =", n, "is", simulate_game(n))
+shots, hits = simulate_game(n)
+print("Number of shots taken to hit the target for n =", n, "is", shots)
+print("Hits:", hits)
